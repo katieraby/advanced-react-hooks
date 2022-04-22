208c05a0884b43d6c946fca0e0ef33493ebeed8b
Katie Raby <katie.raby@icloud.com>
2022-04-22 21:40:50 +0100

    Complete all useReducer exercises + extra credit

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,14 +3,31 @@
 
 import * as React from 'react'
 
+
   // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
-const countReducer = (currentCount, newCount) => newCount
+  // The 2nd argument is referred to as the action - whatever the dispatch func is called with
+  const countReducer = (state, action) => 
+  {
+      switch (action.type) {
+        case 'INCREMENT': {
+          return {count: state.count + action.step}
+      }
+      default: {
+        throw new Error(`Unsupported action type ${action.type}`)
+      }
+    }
+  }
+
 
 function Counter({initialCount = 0, step = 1}) {
-  const [count, setCount] = React.useReducer(countReducer, initialCount)
+  // [state, dispatch] = useReducer(reducer, initial state)
+  const [state, dispatch] = React.useReducer(countReducer, {
+    count: initialCount,
+  })
 
-  const increment = () => setCount(count + step)
+  const {count} = state
+  const increment = () => dispatch({type: 'INCREMENT', step})
+
   return <button onClick={increment}>{count}</button>
 }
 
@@ -18,4 +35,4 @@ function App() {
   return <Counter />
 }
 
-export default App
+export default App;
